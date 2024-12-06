bf3dbb54603848bc1b071e73dcb9594b98d83c04
ReceproF <laburazkuya@hotmail.com>
2024-12-06 21:25:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import json
+import os
 
 app = Flask(__name__)
 
@@ -45,4 +46,6 @@ def update():
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Render için port yapılandırması
+    port = int(os.environ.get("PORT", 5000))  # Render'dan gelen port veya 5000 varsayılan
+    app.run(host='0.0.0.0', port=port, debug=True)
