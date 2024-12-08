87545120ee94de6369e2f6f571ad267778da597e
ReceproF <laburazkuya@hotmail.com>
2024-12-09 00:51:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template    //Bismillahirrahmanirrahim
+from flask import Flask, request, jsonify, render_template    <!-- Bismillahirrahmanirrahim -->
 
 app = Flask(__name__)
 
